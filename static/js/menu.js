efdf6a2306e0ba0a8662c594524f4e6a03796f69
Sean Hammond <seanh@users.noreply.github.com>
2024-06-01 15:22:51 +0100

    Use background-color to highlight current page
    Use a background-color, rather than a bold font-weight, to highlight the
    current page in the nav menu.
    
    Bold doesn't work here because it changes the width of the text, which
    pushes following menu items around when the menu is horizontal on
    smaller screens.
    
    An underline won't work either because the menu itself is already
    underlined on smaller screens, so it'd look weird.
    
    So background color it is.
    
    Also apply this background color to hovered links in the nav menu.
    
    Fixes https://github.com/seanh/sidecar/issues/25

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,5 +1,5 @@
 // Highlight any menu links to the current page.
-for (menuLink of document.getElementById('menu').getElementsByTagName('a')) {
+for (menuLink of document.getElementById('nav').getElementsByTagName('a')) {
   if (menuLink.href === document.URL) {
     menuLink.setAttribute('aria-current', 'page');
   }
