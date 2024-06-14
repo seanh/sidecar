48fd2424877393bd10590d57c52f88fcdefcb27e
Sean Hammond <seanh@users.noreply.github.com>
2024-06-14 18:00:16 +0100

    Show article summaries on index pages
    Make this theme more like a normal Pelican theme by showing article
    summaries on index pages (and showing only titles on the archives page).
    
    Sites that want titles-only on index pages can do this via
    customisation. For example with Pelican configuration you can get rid of
    the index page and move the archives page to "output/index.html".
    And/or you can override some of the theme's templates.
    
    This commit also refactors the CSS to use BEM (Block Element
    Modifier)-style CSS in order to reduce the likelihood of CSS bugs.

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,6 +1,8 @@
 // Highlight any menu links to the current page.
-for (menuLink of document.getElementById('nav').getElementsByTagName('a')) {
-  if (menuLink.href === document.URL) {
-    menuLink.setAttribute('aria-current', 'page');
+for (menu of document.getElementsByClassName('sidebar')) {
+  for (menuLink of menu.getElementsByTagName('a')) {
+    if (menuLink.href === document.URL) {
+      menuLink.setAttribute('aria-current', 'page');
+    }
   }
 }
