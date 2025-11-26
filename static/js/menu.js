9a7fec15ee5aa33292273c24ad8e94011242f91a
Sean Hammond <seanh@users.noreply.github.com>
2025-11-26 14:41:42 +0000

    Redesign navbar

--- static/js/menu.js
+++
@@ -1,8 +0,0 @@
-// Highlight any menu links to the current page.
-for (menu of document.getElementsByClassName('navbar')) {
-  for (menuLink of menu.getElementsByTagName('a')) {
-    if (menuLink.href === document.URL) {
-      menuLink.setAttribute('aria-current', 'page');
-    }
-  }
-}
