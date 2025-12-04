adb90ddc5411f6fc5143a4e29cb3897e6711fa8a
Sean Hammond <seanh@users.noreply.github.com>
2025-12-04 15:07:23 +0000

    Add trailing slashes to URLs
    Something on GitHub Pages seems to enforce these. Not sure if it's a
    built-in behaviour or something I've configured somewhere.

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,49 +21,49 @@ FEED_DOMAIN = SITEURL
 
 # Make the URLs of article pages nicer.
 ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html"
-ARTICLE_URL = "{date:%Y}/{date:%m}/{date:%d}/{slug}"
-ARTICLE_SOURCE_URL = "{article.url}/index{OUTPUT_SOURCES_EXTENSION}"
+ARTICLE_URL = "{date:%Y}/{date:%m}/{date:%d}/{slug}/"
+ARTICLE_SOURCE_URL = "{article.url}index{OUTPUT_SOURCES_EXTENSION}"
 
 # Make the URL of the archives page nicer.
 ARCHIVES_SAVE_AS = "archives/index.html"
-ARCHIVES_URL = "archives"
+ARCHIVES_URL = "archives/"
 
 # Make the URLs of period archive pages nicer.
 YEAR_ARCHIVE_SAVE_AS = "{date:%Y}/index.html"
-YEAR_ARCHIVE_URL = "{date:%Y}"
+YEAR_ARCHIVE_URL = "{date:%Y}/"
 MONTH_ARCHIVE_SAVE_AS = "{date:%Y}/{date:%m}/index.html"
-MONTH_ARCHIVE_URL = "{date:%Y}/{date:%m}"
+MONTH_ARCHIVE_URL = "{date:%Y}/{date:%m}/"
 DAY_ARCHIVE_SAVE_AS = "{date:%Y}/{date:%m}/{date:%d}/index.html"
-DAY_ARCHIVE_URL = "{date:%Y}/{date:%m}/{date:%d}"
+DAY_ARCHIVE_URL = "{date:%Y}/{date:%m}/{date:%d}/"
 
 # Make the URLs of static pages nicer.
 PAGE_SAVE_AS = "{slug}/index.html"
-PAGE_URL = "{slug}"
-PAGE_SOURCE_URL = "{page.url}/index{OUTPUT_SOURCES_EXTENSION}"
+PAGE_URL = "{slug}/"
+PAGE_SOURCE_URL = "{page.url}index{OUTPUT_SOURCES_EXTENSION}"
 
 # Make the URL of the authors page nicer.
 AUTHORS_SAVE_AS = "authors/index.html"
-AUTHORS_URL = "authors"
+AUTHORS_URL = "authors/"
 
 # Make the URLs of author pages nicer.
 AUTHOR_SAVE_AS = "author/{slug}/index.html"
-AUTHOR_URL = "author/{slug}"
+AUTHOR_URL = "author/{slug}/"
 
 # Make the URL of the tags page nicer.
 TAGS_SAVE_AS = "tags/index.html"
-TAGS_URL = "tags"
+TAGS_URL = "tags/"
 
 # Make the URLs of tag pages nicer.
 TAG_SAVE_AS = "tag/{slug}/index.html"
-TAG_URL = "tag/{slug}"
+TAG_URL = "tag/{slug}/"
 
 # Make the URL of the categories page nicer.
 CATEGORIES_SAVE_AS = "categories/index.html"
-CATEGORIES_URL = "categories"
+CATEGORIES_URL = "categories/"
 
 # Make the URLs of category pages nicer.
 CATEGORY_SAVE_AS = "category/{slug}/index.html"
-CATEGORY_URL = "category/{slug}"
+CATEGORY_URL = "category/{slug}/"
 
 SIDECAR_NAVBAR = [
     "HOME",
